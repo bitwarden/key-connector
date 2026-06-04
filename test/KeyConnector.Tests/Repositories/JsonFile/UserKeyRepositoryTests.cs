9942a68a8e7d56491a1e19468d19a393c4692cb5
Maciej Zieniuk <167752252+mzieniukbw@users.noreply.github.com>
2026-06-04 18:13:43 +0200

    [PM-35442] Json user repository not updating (#268)
    * put updates kc key
    
    * integration tests
    
    * PUT needs to upsert
    
    * json user repository not updating
    
    * EF tests include Sql Server, PostgreSql, MySql, MariaDb
    
    * pinned container images, disposable datastore, tostring on id once

---
+++ test/KeyConnector.Tests/Repositories/JsonFile/UserKeyRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Bit.KeyConnector.Repositories;
+using Bit.KeyConnector.Repositories.JsonFile;
+using JsonFlatFileDataStore;
+
+namespace KeyConnector.Tests.Repositories.JsonFile;
+
+public class JsonFileFixture : IUserKeyRepositoryFixture
+{
+    private string _tempDir;
+    private DataStore _dataStore;
+
+    public IUserKeyRepository Repository { get; private set; }
+
+    public Task InitializeAsync()
+    {
+        _tempDir = Path.Combine(Path.GetTempPath(), $"kc-json-test-{Guid.NewGuid()}");
+        Directory.CreateDirectory(_tempDir);
+        var dbPath = Path.Combine(_tempDir, "database.json");
+        _dataStore = new DataStore(dbPath, keyProperty: "--foobar--");
+        Repository = new UserKeyRepository(_dataStore);
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        _dataStore?.Dispose();
+        if (Directory.Exists(_tempDir))
+        {
+            Directory.Delete(_tempDir, true);
+        }
+        return Task.CompletedTask;
+    }
+}
+
+public class UserKeyRepositoryTests : UserKeyRepositoryTestBase<JsonFileFixture>
+{
+    public UserKeyRepositoryTests(JsonFileFixture fixture) : base(fixture) { }
+}
